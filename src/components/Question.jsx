5d82686be1dd4c93421d746a1dc4777cce732df2
Collin Heath <cwhouar15@gmail.com>
2025-11-20 23:04:50 -0800

    you clicked button ${number}

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 function Question() {
+  function handleClick(number){
+  	console.log(`User selected Answer ${number}!`)
+  }
   return (
-    <>
-        <h1> QUESTION HERE </h1>
-        <button> Answer 1</button>
-        <button> Answer 2</button>
-        <button> Answer 3</button>
-        <button> Answer 4</button>
-    </>
+<>
+	<h1> QUESTION HERE </h1>
+<button onClick={()=>handleClick(1)}> Answer 1</button>
+<button onClick={()=>handleClick(2)}> Answer 2</button>
+<button onClick={()=>handleClick(3)}> Answer 3</button>
+<button onClick={()=>handleClick(4)}> Answer 4</button>
+</>
   );
 }
 export default Question;
